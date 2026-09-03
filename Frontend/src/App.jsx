70b8a93db70cf64f97d55b15cb6af8423b9f37c7
Ubuntu <ubuntu@ip-172-31-27-21.ec2.internal>
2026-09-03 12:52:44 +0000

    Configure AWS deployment

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
-const API_URL = "http://localhost:5000";
+const API_URL = "";
 
 function App() {
   const [tasks, setTasks] = useState([]);
